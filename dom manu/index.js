7c6e2985759256b28ea96923b9c12107c9ae1ee9
Anurag shukla <97109151+loanwolf0@users.noreply.github.com>
2023-06-17 13:44:31 +0530

    getElementsByClaasName done

--- dom manu/index.js
+++ dom manu/index.js
@@ -1,6 +1,14 @@
 const header = document.getElementById('main-header');
 const add = document.querySelector('.title');
+const list = document.getElementsByClassName('list-group-item');
 
 header.style.borderBottom = 'solid 3px #000'
 add.style.color = 'green'
 add.innerHTML = '<h1> <b> Add Items </b> </h1>'
+
+
+list[2].style.backgroundColor = 'green' ;
+
+for (let i = 0; i < list.length; i++) {
+    list[i].style.fontWeight = 'bold' ;  
+}
