19d340321287d147603468f832cd59881973210a
Anurag shukla <97109151+loanwolf0@users.noreply.github.com>
2023-06-17 13:06:22 +0530

    Dom manipulation done

---
+++ index.js
@@ -0,0 +1,6 @@
+const header = document.getElementById('main-header');
+const add = document.querySelector('.title');
+
+header.style.borderBottom = 'solid 3px #000'
+add.style.color = 'green'
+add.innerHTML = '<h1> <b> Add Items </b> </h1>'
