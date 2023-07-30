8b76d471832688e0c032a4950ad9f436e145e9b8
Anurag shukla <97109151+loanwolf0@users.noreply.github.com>
2023-07-31 01:36:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
-const header = document.getElementById('main-header');
-const add = document.querySelector('.title');
+// Function to fetch and display the recently added products
+function fetchProducts() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(products => {
+      const productList = document.getElementById('productList');
+      productList.innerHTML = '';
 
-header.style.borderBottom = 'solid 3px #000'
-add.style.color = 'green'
-add.innerHTML = '<h1> <b> Add Items </b> </h1>'
+      products.forEach(product => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Product Name: ${product.title}, Selling Price: $${product.body}`;
+        productList.appendChild(listItem);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching products:', error);
+    });
+}
+
+// Function to add a new product
+function addProduct() {
+  const productName = document.getElementById('productName').value;
+  const sellingPrice = document.getElementById('sellingPrice').value;
+
+  // Prepare the data to be posted
+  const newProduct = {
+    title: productName,
+    body: sellingPrice,
+    userId: 1 // A dummy user ID
+  };
+
+  // Post the data to the API
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newProduct)
+  })
+  .then(response => response.json())
+  .then(() => {
+    // Clear the input fields after successful addition
+    document.getElementById('productName').value = '';
+    document.getElementById('sellingPrice').value = '';
+
+    // Fetch and display the recently added products
+    fetchProducts();
+  })
+  .catch(error => {
+    console.error('Error adding product:', error);
+  });
+}
+
+// Add a click event listener to the "Add" button
+const addButton = document.getElementById('addButton');
+addButton.addEventListener('click', addProduct);
+
+// Fetch and display the recently added products when the page loads
+fetchProducts();
